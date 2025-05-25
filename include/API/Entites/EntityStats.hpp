cbdcc456f9654076b694061bea1dbc2494b3c6b4
Élise Valentin <venodez@gmail.com>
2025-05-26 01:50:32 +0200

    WIP starting of API

---
+++ include/API/Entites/EntityStats.hpp
@@ -0,0 +1,26 @@
+
+#ifndef ENTITYSTATS_HPP_
+    #define ENTITYSTATS_HPP_
+
+    #include "../GameObjectProxy.hpp"
+    #include "../../Game/UGDSCharaParameter.hpp"
+
+class EntityStats : public GameObjectProxy<FGDCharaParameter> {
+    public:
+        void setHP(int32_t hp);
+        void setSP(int32_t sp);
+        void setPhysicalAttack(int32_t physicalAttack);
+        void setMagicalAttack(int32_t magicalAttack);
+        void setPhysicalDefense(int32_t physicalDefense);
+        void setMagicalDefense(int32_t magicalDefense);
+        int32_t getHP(int32_t hp);
+        int32_t getSP(int32_t sp);
+        int32_t getPhysicalAttack();
+        int32_t getMagicalAttack();
+        int32_t getPhysicalDefense();
+        int32_t getMagicalDefense();
+    protected:
+    private:
+};
+
+#endif /* !ENTITYSTATS_HPP_ */
