235a2d9c96b78e1fa92a80e60c15ea31ce132373
Élise Valentin <venodez@gmail.com>
2025-06-17 23:14:00 +0200

    WIP add way to bypass EAC from direct launch

--- include/Patcher/Patches/EventHook.hpp
+++ include/Patcher/Patches/EventHook.hpp
@@ -1,12 +1,12 @@
 #ifndef EVENTHOOK_HPP_
     #define EVENTHOOK_HPP_
 
-    #include "../Patch.hpp"
+    #include "../Trampoline.hpp"
     #include "Hook/EventHandler.hpp"
 
-class EventHook : public Patch {
+class EventHook : public Trampoline {
     public:
-        EventHook(EventType type, uintptr_t target) : Patch(Priority::HIGH, EventTypeNames[type] + "Hook", target, reinterpret_cast<uintptr_t>(EventHandler::callEvent)), _type(type) {};
+        EventHook(EventType type, uintptr_t target) : Trampoline(Priority::HIGH, EventTypeNames[type] + "Hook", target, reinterpret_cast<uintptr_t>(EventHandler::callEvent)), _type(type) {};
         virtual bool apply(uintptr_t baseAddress) override;
         EventType getType() const { return _type; };
     protected:
