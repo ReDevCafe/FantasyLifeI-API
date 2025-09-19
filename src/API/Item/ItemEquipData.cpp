d86070c2e256ce7a95cd6990c1f66828fa0d549d
AmeliaCute <48628282+AmeliaCute@users.noreply.github.com>
2025-09-19 06:15:11 +0100

    Cleaning and Adding functions

--- src/API/Item/ItemEquipData.cpp
+++ src/API/Item/ItemEquipData.cpp
@@ -1,12 +1,27 @@
 #include "API/Item/ItemEquipData.hpp"
+#include "API/Engine/TArrayHelper.hpp"
 #include "Utils.hpp"
+#include "ModLoader.hpp"
 
 std::string ItemEquipData::GetModelIdentifier()
 {
-    return Utils::FNameToString(reinterpret_cast<FGDItemEquipData*>(&this->_object)->modelID);
+    return Utils::FNameToString(static_cast<FGDItemEquipData&>(this->_object).modelID);
 }
 
 void ItemEquipData::SetModel(ItemEquipData item)
 {
-    reinterpret_cast<FGDItemEquipData*>(&this->_object)->modelID = reinterpret_cast<FGDItemEquipData*>(&item.getObject())->modelID;
+    static_cast<FGDItemEquipData&>(this->_object).modelID = static_cast<FGDItemEquipData&>(this->_object).modelID;
+}
+
+TArray<FGDAddSkillLotTable_AddSkillInfo>& ItemEquipData::GetAddSkillLot(int rarity)
+{
+    auto skillIdRaw = TArrayHelper<FName>::Get(static_cast<FGDItemEquipData&>(this->_object).addSkillLotTableList, rarity);
+    std::string skillId = Utils::FNameToString(skillIdRaw);
+
+    return ModLoader::gameCache->GetAddSkillTable(skillId);
+}
+
+void ItemEquipData::SetAddSkillTable(int rarity, FName value)
+{
+    TArrayHelper<FName>::Set(static_cast<FGDItemEquipData&>(this->_object).addSkillLotTableList, rarity, value);
 }
