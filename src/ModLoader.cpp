bb46de93a72e536f021657b1199bed29939f2d19
Amelia <48628282+AmeliaCute@users.noreply.github.com>
2026-04-04 22:55:51 +0100

    feat: enhance build process and console output handling for Wine compatibility

--- src/ModLoader.cpp
+++ src/ModLoader.cpp
@@ -49,9 +49,17 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReser
     {
         case DLL_PROCESS_ATTACH:
         {
-            AllocConsole();
-            freopen_s(reinterpret_cast<FILE**>(stdout), "CONOUT$", "w", stdout);
-            freopen_s(reinterpret_cast<FILE**>(stderr), "CONOUT$", "w", stderr);
+            bool isWine = GetProcAddress(GetModuleHandleA("ntdll.dll"), "wine_get_version") != nullptr;
+            if(isWine)
+            {
+                setvbuf(stdout, nullptr, _IONBF, 0);
+                setvbuf(stderr, nullptr, _IONBF, 0);
+            } else
+            {
+                AllocConsole();
+                freopen_s(reinterpret_cast<FILE**>(stdout), "CONOUT$", "w", stdout);
+                freopen_s(reinterpret_cast<FILE**>(stderr), "CONOUT$", "w", stderr);
+            }            
 
             std::cout.clear();
             std::cerr.clear();
